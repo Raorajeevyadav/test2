d17fd4e64d7c20e3eb0e768e786107a1d6553e32
BA49050 <BA49050@WL2781-0058452.bbby.corp.int>
2021-06-29 13:12:33 +0530

    commit

--- TestJava/src/JavaExample/Inheritance.java
+++ TestJava/src/JavaExample/Inheritance.java
@@ -1,12 +1,32 @@
 package JavaExample;
 
+
+import JavaExample.Animal1.Doge;
+
+class Animal1{
+	
+public static void A(){
+		System.out.println("Method A");
+	}
+
+  static class Doge extends Animal1{
+	
+	public static void A() {
+		System.out.println("Method B");
+	}
+}
+	
+}
+
 public class Inheritance {
 
 	
-	public static void areaofcircle (double r) {
+	public static void main(String[] args) {
 		
-		double area;
-		 area=(22*r*r)/7;
+		Animal1 a= new Animal1();
+		Doge d=  new Doge();
+		a.A();
+		d.A();
 		
 		
 	}
